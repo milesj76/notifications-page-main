d32a6267c52c18f46471b0a36e8d58f9924c2841
Miles Burke <milesj76@gmail.com>
2023-08-10 09:30:01 -0400

    add more content to notifications, reverse order

--- notifications-page-main/src/App.svelte
+++ notifications-page-main/src/App.svelte
@@ -2,14 +2,19 @@
   import Notification from "./lib/Notification.svelte";
   import { notifications } from "./lib/db";
 
-  let unreadNotifications = 3
+  let orderedNotifications = $notifications.reverse()
+  let unreadNotifications = () => {
+    let unread = 0;
+    $notifications.forEach(item => item.read ? null : unread++)
+    return unread
+  }
 </script>
 
 <main class="bg-white mx-4 my-4 max-w-screen">
   <header class="flex justify-between items-center">
     <div class="flex space-x-2">
       <h1 class="font-extrabold text-xl">Notifications</h1>
-      <span class="badge badge-secondary badge-lg rounded-md text-base py-3 bg-blue-base text-white">{unreadNotifications}</span>
+      <span class="badge badge-secondary badge-lg rounded-md text-base py-3 bg-blue-base text-white">{unreadNotifications()}</span>
     </div>
 
     <div>
@@ -19,7 +24,7 @@
 
   <!-- Notifications -->
   <section class="space-y-3"> 
-    {#each $notifications as notification}
+    {#each orderedNotifications as notification}
       <Notification {notification} />
     {/each}
   </section>
--- notifications-page-main/src/lib/Notification.svelte
+++ notifications-page-main/src/lib/Notification.svelte
@@ -1,7 +1,24 @@
 <script lang="ts">
-  export let notification
+  export let notification;
+  let { userName, userAvatar, datetime, read, eventType, event } = notification
+  let eventDetails = determineEvent(eventType, event);
+  
+  function determineEvent(type, eventObj) {
+    // return eventObj
+  }
+
 </script>
 
 <article class="p-2 bg-blue-100 rounded-md">
-  <p>{notification.userName}</p>
+  <!-- Avatar -->
+  <img src={userAvatar} alt="avatar">
+
+  <!-- Details -->
+  <div>
+    <p><span>{userName}</span> {eventDetails} <span class="" /></p>
+
+    <!-- Date -->
+    <div><p>{datetime}</p></div>
+  </div>
+
 </article>
